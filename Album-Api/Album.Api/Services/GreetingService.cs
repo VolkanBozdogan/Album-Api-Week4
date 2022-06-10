38e560a9c0bfdb515dc6f67f104e7df8138b429a
mrcocchieman <72669386+mrcocchieman@users.noreply.github.com>
2022-06-10 14:32:59 +0200

    string?

--- Album-Api/Album.Api/Services/GreetingService.cs
+++ Album-Api/Album.Api/Services/GreetingService.cs
@@ -8,7 +8,7 @@ namespace Album.Api.Services
 {
     public class GreetingService
     {
-        public string GetGreeting(string? name = "Salar")
+        public string GetGreeting(string? name = "World")
         {
             Program.Log($"GetGreeting method, parameter name = {name}");
             if (string.IsNullOrWhiteSpace(name) || string.IsNullOrEmpty(name))
