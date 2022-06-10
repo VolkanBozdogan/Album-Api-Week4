e46ecda5e8b1df0ebd301e3d4e75c4a456b927fc
mrcocchieman <72669386+mrcocchieman@users.noreply.github.com>
2022-06-10 15:06:09 +0200

    update

--- Album-Api/Album.Api/Services/GreetingService.cs
+++ Album-Api/Album.Api/Services/GreetingService.cs
@@ -13,7 +13,7 @@ namespace Album.Api.Services
             Program.Log($"GetGreeting method, parameter name = {name}");
             if (string.IsNullOrWhiteSpace(name) || string.IsNullOrEmpty(name))
             {
-                return $"Hello Worldtest";
+                return $"Hello World";
             }
             Program.Log($"Return GetGreeting method : Hello {name}");
             return $"Hello {name} from {Dns.GetHostName()} v2";
